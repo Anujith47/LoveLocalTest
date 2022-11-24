4ec45ddc1738d77e9218695d31806ce8de179d6e
Anujith C <anujithc@pop-os.localdomain>
2022-11-24 18:06:41 +0530

    moved mongo credentials to env variables

--- order_app/__init__.py
+++ order_app/__init__.py
@@ -4,16 +4,20 @@ import json
 from flask import Flask, request, jsonify
 from flask_mongoengine import MongoEngine
 
+from .settings import (
+    MONGO_USER, MONGO_PASS, MONGO_DATABASE, MONGO_PORT, MONGO_HOST)
+
 
 # create and configure the app
 app = Flask(__name__)
 
+# configure mongodb settings
 app.config['MONGODB_SETTINGS'] = {
-    'db': 'lovedb',
-    'host': 'mongo',
-    'port': 27017,
-    'username': 'root',
-    'password': 'pass'
+    'db': MONGO_DATABASE,
+    'host': MONGO_HOST,
+    'port': int(MONGO_PORT),
+    'username': MONGO_USER,
+    'password': MONGO_PASS
 }
 db = MongoEngine()
 db.init_app(app)
@@ -24,10 +28,11 @@ try:
 except OSError:
     pass
 
+# register blueprint
 from . import order
 app.register_blueprint(order.bp)
 
-
+# registering init-db custom script
 from .db import init_app
 init_app(app)
 
